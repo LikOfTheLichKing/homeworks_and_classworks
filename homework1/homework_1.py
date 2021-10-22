87aebe17a9147bdc7dcce8b3bb37c3946fcc55ae
LikOfTheLichKing <yummigame@gmail.com>
2021-10-22 18:44:11 +0300

    Update homework_1.py

--- homework1/homework_1.py
+++ homework1/homework_1.py
@@ -2,43 +2,95 @@ import string
 import random
 
 
-cars_names = ["Aston Martin", "Audi", "BMW", "Cadillac", "Chevrolet", "Datsun", "Ferrari", "Ford", "Honda", "Mercedes", "Nissan", "Porsche", "Rolls-Royce", "Toyota", "Volkswagen", "Subaru"]
+cars_names = [
+    "Aston Martin",
+    "Audi",
+    "BMW",
+    "Cadillac",
+    "Chevrolet",
+    "Datsun",
+    "Ferrari",
+    "Ford",
+    "Honda",
+    "Mercedes",
+    "Nissan",
+    "Porsche",
+    "Rolls-Royce",
+    "Toyota",
+    "Volkswagen",
+    "Subaru",
+]
+
+def random_vin_number() -> str:
+    vin_number = "".join(
+        random.choices(string.ascii_uppercase + string.digits, k=17)
+    )
+    return vin_number
+
+def random_name() -> str:
+    name = random.choice(cars_names)
+    return name
 
 class Car:
-    def __init__(self, model: str):
-        self.model = model
-        self.vin_number = "".join(random.choices(string.ascii_uppercase + string.digits, k=17))
-        self.name = cars_names[random.randint(0, len(cars_names)-1)]
 
+    def __init__(self, model: str, vin_number: str, name: str) -> None:
+        self.model = model
+        self.vin_number = vin_number
+        self.name = name
+
+        if self.name is None:
+            self.set_name(random_name())
+
+        if self.vin_number is None:
+            self.set_vin_number(random_vin_number())
+
+    def set_vin_number(self, vin_number) -> None:
+        self.vin_number = vin_number
+
+    def set_name(self, name) -> None:
+        self.name = name
 
 class Human:
-    def __init__(self, name: str, age: int, cars: list): #cars list will contain elements with class: Car
+
+    def __init__(self, name: str, age: int, cars: list[Car]):
 
         self.name = name
         self.age = age
         self.cars = cars
-        self.current_car = None
+        self.current_car = None   # В моей версии python еще нету Optional
 
-    def choose_current_car(self):
-        number_in_cars_list = 0
-        output_str = "Choose new current car for "+self.name+" (enter number). Cars list:\n"
-
-        for i in range(0,len(self.cars)):
-            output_str += str(i+1)+") "+self.cars[i].name+"\n"
-
-        while number_in_cars_list < 1 or number_in_cars_list > len(self.cars):
-            number_in_cars_list = int(input(output_str))
-
-        self.current_car = self.cars[number_in_cars_list-1].name
-        print("new current car:"+self.cars[number_in_cars_list-1].name)
+    def set_current_car(self, number_of_car):
+        if number_of_car >= 0 or number_of_car >= len(self.cars)-1:
+            self.current_car = self.cars[number_of_car]
+        else:
+            raise ValueError("No such cars")
 
 
-all_cars = []
-for i in range(0,10):
-    all_cars.append(Car(model="AM20"))
+def choose_current_car(human: Human) -> None:
+    output_str = ("Choose new current car for " + human.name + " (enter number). Cars list:")
 
-yuriy = Human(name="Yuriy" , age=42, cars=all_cars[0:5])
+    for i in range(len(human.cars)):
+        output_str += "\n" + str(i+1) + ") " + str(human.cars[i].name)
+
+    number_of_car = int(input(output_str+"\n"))-1
+
+    try:
+        human.set_current_car(number_of_car)
+        print("New current car: "+human.current_car.name)
+    except ValueError:
+        print("Incorrect number")
+
+
+all_cars = [Car(model="AM20", vin_number=None, name=None) for i in range(0, 10)]
+
+yuriy = Human(name="Yuriy", age=42, cars=all_cars[0:5])
 platon = Human(name="Platon", age=35, cars=all_cars[5:10])
 
-yuriy.choose_current_car()
-platon.choose_current_car()
+def main() -> None:
+    choose_current_car(yuriy)
+    choose_current_car(platon)
+
+if __name__ == "__main__":
+    main()
+
+
