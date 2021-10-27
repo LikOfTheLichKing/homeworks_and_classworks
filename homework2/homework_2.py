200329548699ec7ad05b30e01552b6a3ffcab7b4
LikOfTheLichKing <yummigame@gmail.com>
2021-10-27 17:44:07 +0300

    v 1.0

---
+++ homework2/homework_2.py
@@ -0,0 +1,24 @@
+from datetime import date
+from flask import Flask
+from flask import render_template
+from typing import Optional
+import json
+
+
+app = Flask(__name__)
+file_data: Optional[dict[date, list[tuple[str, str]]]] = None
+
+with open("data.json") as file:
+    file_data: [date, list[tuple[str, str]]] = json.load(file)
+
+all_keys = file_data.keys()
+@app.route("/", methods=["GET"])
+def get_list():
+    global file_data
+    return render_template("main.jinja", schedule=file_data)
+
+def main() -> None:
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
