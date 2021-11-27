182443528c60dcd8df570ff26b4f5a0f6d619130
LikOfTheLichKing <yummigame@gmail.com>
2021-11-27 11:47:05 +0200

    v 1.0

--- homework5/passwords.py
+++ homework5/passwords.py
@@ -23,9 +23,9 @@ class PasswordHandler:
         hash_data = self.hash(password.encode("utf-8"))
         return self.encode(hash_data.digest()).decode("utf-8")
 
-    def hash_password(self) -> str:
+    def hash_password(self, password: str) -> str:
         salt = self.get_salt()
-        return self.hash_password_raw(self.password + ":" + salt) + ":" + salt
+        return self.hash_password_raw(password + ":" + salt) + ":" + salt
 
     def get_salt(self) -> str:
         salt_number = random.randint(0, 2 ** 256 - 1)
@@ -37,6 +37,6 @@ class PasswordHandler:
 
 
 password = PasswordHandler(
-    password="1234", hash_method=hashlib.sha512, encode_method=base64.b32encode
+    hash_method=hashlib.sha512, encode_method=base64.b32encode, pepper=None
 )
-print(password.hash_password())
+print(password.hash_password(password="1234"))
